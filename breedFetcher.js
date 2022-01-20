266da9a46ca93cb2e6658383811b127f1ab2fee5
Fransiskus <aldzaidea@gmail.com>
2022-01-20 22:10:44 +0000

    add index.js to get callback function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,31 @@
 const request = require("request");
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`,
-  (error, response, body) => {
-    if (error) {
-      console.log("Error :", error);
-      return;
-    } else {
-      const data = JSON.parse(body);
-      if (data[0] === undefined) {
-        console.log("The breed is not found");
+const fetchBreedDescription = (breedName, callback) => {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        console.log("Error :", error);
         return;
       } else {
-        console.log(
-          "Name:",
-          data[0]["name"],
-          "\n",
-          "Description:",
-          data[0]["description"]
-        );
-        return;
+        const data = JSON.parse(body);
+
+        if (data[0] === undefined) {
+          console.log("The breed is not found");
+          return;
+        } else {
+          console.log(
+            "Name:",
+            data[0]["name"],
+            "\n",
+            "Description:",
+            data[0]["description"]
+          );
+        }
       }
+      callback();
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
